c9306344be9e1b30353fba4e0449cf672973b061
MaximeBonnet27 <maximebonnet27@gmail.com>
2015-04-02 17:17:59 +0200

    Commencé dot files

--- src/cpa/automate/Automate.java
+++ src/cpa/automate/Automate.java
@@ -10,119 +10,133 @@ import java.util.ArrayList;
 
 public class Automate implements Serializable {
 
-  private Etat etatInitial;
-  private ArrayList<Etat> etatsFinaux;
-  private ArrayList<Etat> etats;
+	private Etat etatInitial;
+	private ArrayList<Etat> etatsFinaux;
+	private ArrayList<Etat> etats;
 
-  public Automate(Etat etatInitial, ArrayList<Etat> etatsFinaux,
-      ArrayList<Etat> etats) {
-    super();
-    this.etatInitial = etatInitial;
-    this.etatsFinaux = etatsFinaux;
-    this.etats = etats;
-  }
-  
-  public boolean verifierMot(String mot){
-    return verifierMotRecurence(mot, etatInitial, 0);  
-  }
+	public Automate(Etat etatInitial, ArrayList<Etat> etatsFinaux,
+			ArrayList<Etat> etats) {
+		super();
+		this.etatInitial = etatInitial;
+		this.etatsFinaux = etatsFinaux;
+		this.etats = etats;
+	}
 
-  private boolean verifierMotRecurence(String mot, Etat etatActuel, int indexActuel){
-    if(isEtatFinal(etatActuel)){
-      return true;
-    }
-    if(indexActuel == mot.length()){
-      for(Transition transition : etatActuel.getTransitions()){
-        if(transition.isEpsilonTransition()){
-          return verifierMotRecurence(mot, transition.getArrivee(), indexActuel);
-        }
-        if(transition.isFinTransition()){
-          return true;
-        }
-      }
-      return false;
-    }
-    else{
-      char caractere = mot.charAt(indexActuel);
-      boolean match = false;
-      for(Transition transition : etatActuel.getTransitions()){
-        if(transition.estDansEtiquette(caractere)){
-          match = match || verifierMotRecurence(mot, transition.getArrivee(), indexActuel + 1);
-        }
-        else if(transition.isEpsilonTransition()){
-          match = match || verifierMotRecurence(mot, transition.getArrivee(), indexActuel);
-        }
-        else if(transition.isPointTransition()){
-          match = match || verifierMotRecurence(mot, transition.getArrivee(), indexActuel + 1);
-        }
-        else if(transition.isDebutTransition()){
-          match = match || (indexActuel == 0) && verifierMotRecurence(mot, transition.getArrivee(), indexActuel);
-        }
-      }
-      if (!match){
-        return verifierMotRecurence(mot, etatInitial, indexActuel+1);
-      }
-      else return true;
-    }
-  }
+	public boolean verifierMot(String mot){
+		return verifierMotRecurence(mot, etatInitial, 0);  
+	}
 
-  public Etat getEtatInitial() {
-    return etatInitial;
-  }
+	private boolean verifierMotRecurence(String mot, Etat etatActuel, int indexActuel){
+		if(isEtatFinal(etatActuel)){
+			return true;
+		}
+		if(indexActuel == mot.length()){
+			for(Transition transition : etatActuel.getTransitions()){
+				if(transition.isEpsilonTransition()){
+					return verifierMotRecurence(mot, transition.getArrivee(), indexActuel);
+				}
+				if(transition.isFinTransition()){
+					return true;
+				}
+			}
+			return false;
+		}
+		else{
+			char caractere = mot.charAt(indexActuel);
+			boolean match = false;
+			for(Transition transition : etatActuel.getTransitions()){
+				if(transition.estDansEtiquette(caractere)){
+					match = match || verifierMotRecurence(mot, transition.getArrivee(), indexActuel + 1);
+				}
+				else if(transition.isEpsilonTransition()){
+					match = match || verifierMotRecurence(mot, transition.getArrivee(), indexActuel);
+				}
+				else if(transition.isPointTransition()){
+					match = match || verifierMotRecurence(mot, transition.getArrivee(), indexActuel + 1);
+				}
+				else if(transition.isDebutTransition()){
+					match = match || (indexActuel == 0) && verifierMotRecurence(mot, transition.getArrivee(), indexActuel);
+				}
+			}
+			if (!match){
+				return verifierMotRecurence(mot, etatInitial, indexActuel+1);
+			}
+			else return true;
+		}
+	}
 
-  public ArrayList<Etat> getEtatsFinaux() {
-    return etatsFinaux;
-  }
+	public Etat getEtatInitial() {
+		return etatInitial;
+	}
 
-  public ArrayList<Etat> getEtats() {
-    return etats;
-  }
+	public ArrayList<Etat> getEtatsFinaux() {
+		return etatsFinaux;
+	}
+
+	public ArrayList<Etat> getEtats() {
+		return etats;
+	}
 
 
-  public boolean isEtatFinal(Etat etat){
-    return etatsFinaux.contains(etat);
-  }
+	public boolean isEtatFinal(Etat etat){
+		return etatsFinaux.contains(etat);
+	}
 
-  @Override
-  public String toString() {
-    StringBuilder builder = new StringBuilder();
-    for(Etat e : etats){
-      if(e == etatInitial){
-        builder.append("-> ");
-      }
-      if(etatsFinaux.contains(e)){
-        builder.append("* ");
-      }
-      builder.append(e.toString());
-      builder.append("\n");
-    }
-    return builder.toString();
-  }
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		for(Etat e : etats){
+			if(e == etatInitial){
+				builder.append("-> ");
+			}
+			if(etatsFinaux.contains(e)){
+				builder.append("* ");
+			}
+			builder.append(e.toString());
+			builder.append("\n");
+		}
+		return builder.toString();
+	}
 
-  @Override
-  public Object clone() {
-    Object obj = null;
-    try {
-        // Write the object out to a byte array
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        ObjectOutputStream out = new ObjectOutputStream(bos);
-        out.writeObject(this);
-        out.flush();
-        out.close();
+	@Override
+	public Object clone() {
+		Object obj = null;
+		try {
+			// Write the object out to a byte array
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			ObjectOutputStream out = new ObjectOutputStream(bos);
+			out.writeObject(this);
+			out.flush();
+			out.close();
 
-        // Make an input stream from the byte array and read
-        // a copy of the object back in.
-        ObjectInputStream in = new ObjectInputStream(
-            new ByteArrayInputStream(bos.toByteArray()));
-        obj = in.readObject();
-    }
-    catch(IOException e) {
-        e.printStackTrace();
-    }
-    catch(ClassNotFoundException cnfe) {
-        cnfe.printStackTrace();
-    }
-    return obj;  }
-  
-  
-  
+			// Make an input stream from the byte array and read
+			// a copy of the object back in.
+			ObjectInputStream in = new ObjectInputStream(
+					new ByteArrayInputStream(bos.toByteArray()));
+			obj = in.readObject();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		catch(ClassNotFoundException cnfe) {
+			cnfe.printStackTrace();
+		}
+		return obj; 
+       	
+	}
+
+	public String toDotFile(){
+		StringBuilder sb = new StringBuilder();
+		sb.append("digraph automate{\n");
+		sb.append("size=\"8,10\";");
+		for(Etat e : etats){
+			sb.append(e.toDotFile());
+		}
+		sb.append("}\n");
+		return sb.toString();
+
+	}
+
+
+
 }
--- src/cpa/automate/Etat.java
+++ src/cpa/automate/Etat.java
@@ -5,65 +5,70 @@ import java.util.ArrayList;
 
 public class Etat implements Serializable{
 
-  private static int compteur = 0;
-  private int numero;
-  private ArrayList<Transition> transitions;
+	private static int compteur = 0;
+	private int numero;
+	private ArrayList<Transition> transitions;
 
-  public Etat() {
-    numero = compteur++;
-    transitions = new ArrayList<Transition>();
-  }
-  
-  
-  
-  public void addTransition(Transition t) {
-    transitions.add(t);
-  }
+	public Etat() {
+		numero = compteur++;
+		transitions = new ArrayList<Transition>();
+	}
 
-  public ArrayList<Transition> getTransitions() {
-    return transitions;
-  }  
-  
-  public String toString(){
-    StringBuilder builder = new StringBuilder();
-    builder.append(numero + " {");
-    for(Transition transition : transitions){
-      if(transition.isEpsilonTransition()){
-        builder.append(" £=>" + transition.getArrivee().getNumero() );
-      }
-      else if(transition.isPointTransition()){
-        builder.append(" .=>" + transition.getArrivee().getNumero() );
 
-      }
-      else if(transition.isDebutTransition()){
-        builder.append(" ^=>" + transition.getArrivee().getNumero() );
-      }
-      else if(transition.isFinTransition()){
-        builder.append(" $=>" + transition.getArrivee().getNumero() );
-      }
-      else{
-      builder.append(" " + transition.getEtiquette() + "=>" + transition.getArrivee().getNumero() );
-      }
-    }
-    builder.append(" }");
-    return builder.toString();
-  }
-  
-  public int getNumero() {
-    return numero;
-  }
 
-  @Override
-  public Object clone(){
-    Etat clone = new Etat();
-    clone.numero = numero;
-    for(Transition t : transitions){
-      clone.addTransition(new Transition(clone, (Etat) t.getArrivee().clone(), t.getEtiquette()));
-    }
-    return clone;
-  }
+	public void addTransition(Transition t) {
+		transitions.add(t);
+	}
 
-  
-  
-  
+	public ArrayList<Transition> getTransitions() {
+		return transitions;
+	}  
+
+	public String toString(){
+		StringBuilder builder = new StringBuilder();
+		builder.append(numero + " {");
+		for(Transition transition : transitions){
+			if(transition.isEpsilonTransition()){
+				builder.append(" £=>" + transition.getArrivee().getNumero() );
+			}
+			else if(transition.isPointTransition()){
+				builder.append(" .=>" + transition.getArrivee().getNumero() );
+
+			}
+			else if(transition.isDebutTransition()){
+				builder.append(" ^=>" + transition.getArrivee().getNumero() );
+			}
+			else if(transition.isFinTransition()){
+				builder.append(" $=>" + transition.getArrivee().getNumero() );
+			}
+			else{
+				builder.append(" " + transition.getEtiquette() + "=>" + transition.getArrivee().getNumero() );
+			}
+		}
+		builder.append(" }");
+		return builder.toString();
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	@Override
+	public Object clone(){
+		Etat clone = new Etat();
+		clone.numero = numero;
+		for(Transition t : transitions){
+			clone.addTransition(new Transition(clone, (Etat) t.getArrivee().clone(), t.getEtiquette()));
+		}
+		return clone;
+	}
+
+	public String toDotFile(){
+		StringBuilder sb = new StringBuilder();
+		for(Transition t : transitions){
+			sb.append("S"+numero+" -> S" +t.getArrivee().getNumero()+ "[color=black, style=solid, label=\""+t.getEtiquette()+"\"];\n");
+		}
+		return sb.toString();
+	}
+
 }
